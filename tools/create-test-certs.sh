0a22e5cf4bf769798fe72a9620602a5ce958a6fe
Lorenzo Manacorda <lorenzo@mailbox.org>
2017-03-02 10:15:56 +0100

    move cert generation in for loop

--- tools/create-test-certs.sh
+++ tools/create-test-certs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+
 set -e
 
 CERTS_DIR=certs
 KEY_SIZE=512
-DAYS=365
 
 mkdir -p $CERTS_DIR $CERTS_DIR/calico $CERTS_DIR/etcd
 
@@ -11,15 +11,12 @@ cd $CERTS_DIR || exit 1
 
 keys=(apiserver worker calico/client etcd/server)
 
-# generate master keys
 for key in ${keys[*]}; do
+  # generate master keys
   openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:$KEY_SIZE -out "${key}-ca.pem"
-done
 
-# generate self-signed certs and cert keys
-# -nodes: unencrypted key
-# -x509: self-signed
-openssl req -new -newkey rsa:$KEY_SIZE -days $DAYS -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout apiserver-key.pem -out apiserver.pem
-openssl req -new -newkey rsa:$KEY_SIZE -days $DAYS -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout worker-key.pem -out worker.pem
-openssl req -new -newkey rsa:$KEY_SIZE -days $DAYS -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout calico/client-key.pem -out calico/client.pem
-openssl req -new -newkey rsa:$KEY_SIZE -days $DAYS -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout etcd/server-key.pem -out etcd/server.pem
+  # generate self-signed certs and cert keys
+  # -nodes: unencrypted key
+  # -x509: self-signed
+  openssl req -new -newkey rsa:$KEY_SIZE -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout "${key}-key.pem" -out "${key}.pem"
+done
